c615a52d30584e5dd40478d7a7fc8e904d2ddcd8
DedaLenya <ip66@bk.ru>
2020-11-03 20:01:17 +0500

    чистим рабочий файл

--- les1_31.10.20/cw1_1.py
+++ les1_31.10.20/cw1_1.py
@@ -1,22 +1,9 @@
-# print('*'*40)
-# a = float(input('Please inter a: '))
-# b = float(input('Please inter b: '))
-#
-# day = 1
-# print(f'{day} day - {a:.2f}')
-# while a < b:
-#     day += 1
-#     a *= 1.1  # short version of a = a + a*0.1
-#     print(f'{day} day - {a:.2f}')
-#
-# print('Answer is:', day)
-
-a = int(input("Результат за день в км>>"))
-b = int(input("Результат ожидаемый в км>>"))
-d = 1
-print(d, "-й день:", round(a, 2))
-while a < b:
-    d += 1
-    a *= 1.1
-    print(d, "-й день:", round(a, 2))
-print("На", d, "-й день ты достигнешь нужного результата")
+string = input("Введите любую строку")
+i = 0
+while i < len(string):
+    if string[i:i+2] == "##":
+        break
+    print(string[i])
+    i += 1
+else:
+    print('Проверено')
