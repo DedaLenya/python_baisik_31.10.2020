4b98bf7d41bb3f55ffeac00a70e71b2bd1969b8d
DedaLenya <ip66@bk.ru>
2020-11-03 19:57:59 +0500

    Все дни важны

--- les1_31.10.20/cw1_1.py
+++ les1_31.10.20/cw1_1.py
@@ -1,1 +1,22 @@
-print()
+# print('*'*40)
+# a = float(input('Please inter a: '))
+# b = float(input('Please inter b: '))
+#
+# day = 1
+# print(f'{day} day - {a:.2f}')
+# while a < b:
+#     day += 1
+#     a *= 1.1  # short version of a = a + a*0.1
+#     print(f'{day} day - {a:.2f}')
+#
+# print('Answer is:', day)
+
+a = int(input("Результат за день в км>>"))
+b = int(input("Результат ожидаемый в км>>"))
+d = 1
+print(d, "-й день:", round(a, 2))
+while a < b:
+    d += 1
+    a *= 1.1
+    print(d, "-й день:", round(a, 2))
+print("На", d, "-й день ты достигнешь нужного результата")
